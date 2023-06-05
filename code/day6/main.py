efc942d7340055d42ea36c365bd7cb189a787d5c
Astrin64 <ameendesilva@gmail.com>
2023-06-05 12:30:24 -0500

    day6 - Functions

---
+++ code/day6/main.py
@@ -0,0 +1,42 @@
+#day6 - Functions & Karel
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+#Karel - reeborg.ca - Hurdle 1
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# def hurdle():
+#     move()
+#     jump()
+
+# hurdle()
+# hurdle()
+# hurdle()
+# hurdle()
+# hurdle()
+# hurdle()
+#OR
+# for step in range(6):
+#     hurdle()
+#OR
+#use a While loops
+# num_of_hurdles = 6
+# while num_of_hurdles > 0:
+#     jump()
+#     num_of_hurdles -=1
+
+#Karel - reeborg.ca - Hurdle 2
