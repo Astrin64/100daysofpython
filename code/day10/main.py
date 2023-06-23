1ed534a5a05aa5b289a8d8bc96fbf4ddbd39cf06
Astrin64 <ameendesilva@gmail.com>
2023-06-23 16:31:20 -0500

    day10 - Functions with outputs

--- code/day10/main.py
+++ code/day10/main.py
@@ -9,6 +9,7 @@ def my_function():
 
 
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case of the name"""
     if f_name == "" or l_name == "":
         return
     formatted_f_name = f_name.title()
@@ -18,3 +19,7 @@ def format_name(f_name, l_name):
 
 format_name("aMEeN", "DE SILVA")
 print(format_name(input("What is your first name? \n"), input("What is your last name? \n")))
+
+#Docstrings
+#adds the first multiline comment in the first indented line under a function like line 12
+
